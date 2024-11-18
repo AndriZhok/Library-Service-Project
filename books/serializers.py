a6c7b578d156acf3933d4bda9d4153413d82c440
Andriy Zhuk <andri195595@gmail.com>
2024-11-18 20:38:19 +0200

    added book list and book detail logic in app

--- books/serializers.py
+++ books/serializers.py
@@ -7,3 +7,15 @@ class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ("title", "author", "cover", "inventory", "daily_fee")
+
+
+class BookListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author")
+
+
+class BookDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ("id", "title", "author", "cover", "inventory", "daily_fee")
--- books/views.py
+++ books/views.py
@@ -1,9 +1,19 @@
 from rest_framework import viewsets
 
 from books.models import Book
-from books.serializers import BookSerializer
+from books.serializers import BookSerializer, BookDetailSerializer, BookListSerializer
 
 
 class BookListView(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    action_serializer_classes = {
+        "list": BookListSerializer,
+        "retrieve": BookDetailSerializer,
+    }
+
+    def get_serializer_class(self):
+        return self.action_serializer_classes.get(self.action, self.serializer_class)
+
+    def get_queryset(self):
+        return super().get_queryset()
